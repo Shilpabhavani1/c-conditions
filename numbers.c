1ef8450b4a0f17700516c332139f1ba8b093b64d
Shilpabhavani1 <96565325+Shilpabhavani1@users.noreply.github.com>
2021-12-30 15:50:11 +0530

    Add files via upload

---
+++ numbers.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void main()
+{
+	
+	int day;
+	printf("enter the number");
+	scanf("%d",&day);
+	if(day==0)
+	{
+		printf ("zero");
+	}
+             else if(day==1)
+	 {    
+	       printf("one");
+              }
+              else if(day==2)
+              {
+              	printf("two");
+	 }
+	 else if(day==3)
+	 {
+	 	printf("three");
+	 }
+	 else if(day==4)
+	 {
+	 	printf("four");
+	 }
+	 else if(day==5)
+	 {
+	 	printf("five");
+	 }
+	 else if(day==6)
+	 {
+	 	printf("six");
+	 }
+	 else if(day==7)
+	 {
+	 	printf("seven");
+			    }	   	
+}
