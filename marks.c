4908077214d54872dadb7f9943dfeeeae010b537
Shilpabhavani1 <96565325+Shilpabhavani1@users.noreply.github.com>
2021-12-31 13:19:29 +0530

    Add files via upload

--- marks.c
+++ marks.c
@@ -4,27 +4,27 @@ void main()
 	int marks;
 	printf("ener marks");
 	scanf("%d",&marks);
-	if(90<=marks<=100)
+	if(90<=marks && marks<=100)
 	{
 		printf("o grade");
 		
 	}
-	else if(80<=marks<90)
+	else if(80<=marks && marks<90)
 	{
 		printf("A grade");
 		
 	}
-	else if(70<=marks<80)
+	else if(70<=marks && marks<80)
 	{
 		printf("B grade");
 		
 	}
-	else if(60<=marks<70)
+	else if(60<=marks && marks<70)
 	{
 		printf("C grade");
 		
 	}
-	else if (50<=marks<60)
+	else if (50<=marks && marks<60)
 	{
 		printf("D grade");
 	}
