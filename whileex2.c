0d6e4cae61fa59ca84fcfa95cf579c88fce9078f
Shilpabhavani1 <96565325+Shilpabhavani1@users.noreply.github.com>
2022-01-06 14:10:30 +0530

    Add files via upload

---
+++ whileex2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int i=10;
+	
+	while(i<=17)
+	{
+		printf("%d",i);
+		i++;
+	}
+}
---
+++ whileex3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int i=215;
+	while(i<=219)
+	{
+		printf("%d",i);
+		i++;
+	}
+}
